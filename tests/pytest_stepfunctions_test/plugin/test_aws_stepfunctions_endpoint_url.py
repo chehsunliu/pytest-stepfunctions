9fb2decbfe10e65981dea7737066565273c9310a
chehsunliu <chehsunliu@gmail.com>
2020-07-05 01:31:04 +0800

    Add test for a state machine with boto3 client

--- tests/pytest_stepfunctions_test/plugin/test_aws_stepfunctions_endpoint_url.py
+++ tests/pytest_stepfunctions_test/plugin/test_aws_stepfunctions_endpoint_url.py
@@ -1,30 +1,12 @@
 import json
-import time
-from typing import Any, Dict
-from uuid import uuid4
+from string import Template
+from typing import Any, Dict, List
 
 import boto3
+import pytest
+from botocore.stub import Stubber
 
-
-class StepFunctionsRunner:
-    def __init__(self, endpoint_url: str):
-        self.endpoint_url: str = endpoint_url
-
-    def run(self, definition: str, input_string: str) -> Dict[str, Any]:
-        sfn_client = boto3.client("stepfunctions", endpoint_url=self.endpoint_url)
-
-        state_machine_arn = sfn_client.create_state_machine(
-            name=str(uuid4()), definition=definition, roleArn="arn:aws:iam::012345678901:role/DummyRole",
-        )["stateMachineArn"]
-        execution_arn: str = sfn_client.start_execution(
-            stateMachineArn=state_machine_arn, name=str(uuid4()), input=input_string
-        )["executionArn"]
-
-        while True:
-            response = sfn_client.describe_execution(executionArn=execution_arn)
-            if response["status"] != "RUNNING":
-                return response
-            time.sleep(0.5)
+from pytest_stepfunctions_test.utility import StepFunctionsRunner
 
 
 def test_simple_state_machine(aws_stepfunctions_endpoint_url: str) -> None:
@@ -48,3 +30,61 @@ def test_simple_state_machine(aws_stepfunctions_endpoint_url: str) -> None:
 
     assert "SUCCEEDED" == response["status"]
     assert {"append": {"text": "abc"}, "n": 123} == json.loads(response["output"])
+
+
+@pytest.fixture
+def mock_emr_client_stubber(mocker: Any) -> Stubber:
+    real_emr_client = boto3.client("emr")
+    stubber: Stubber = Stubber(real_emr_client)
+    mocker.patch(__name__ + ".boto3").client.return_value = real_emr_client
+
+    return stubber
+
+
+def list_emr_cluster_ids(event: Dict[str, Any], *_args: Any, **_kwargs: Any) -> List[str]:
+    emr_client = boto3.client("emr")
+    response = emr_client.list_clusters(ClusterStates=event["states"])
+
+    if "Clusters" not in response:
+        return []
+
+    return [item["Id"] for item in response["Clusters"]]
+
+
+def test_boto3_involved_state_machine(aws_stepfunctions_endpoint_url: str, mock_emr_client_stubber: Stubber) -> None:
+    lambda_arn_base: str = "arn:aws:lambda:us-east-1:123456789012:function:"
+    list_emr_cluster_ids_lambda_arn: str = (
+        lambda_arn_base + list_emr_cluster_ids.__module__ + "." + list_emr_cluster_ids.__name__
+    )
+
+    definition: str = Template(
+        """
+        {
+          "StartAt": "ListEMRClusterIds",
+          "States": {
+            "ListEMRClusterIds": {
+              "Type": "Task",
+              "Resource": "${ListEMRClusterIdsLambdaArn}",
+              "ResultPath": "$.emr_cluster_ids",
+              "End": true
+            }
+          }
+        }
+        """,
+    ).safe_substitute(ListEMRClusterIdsLambdaArn=list_emr_cluster_ids_lambda_arn)
+    input_string = '{"states": ["RUNNING", "WAITING"]}'
+
+    service_response = {"Clusters": [{"Id": "j-0000000"}, {"Id": "j-0000001"}, {"Id": "j-0000002"}]}
+    expected_params = {"ClusterStates": ["RUNNING", "WAITING"]}
+    mock_emr_client_stubber.add_response(
+        "list_clusters", service_response=service_response, expected_params=expected_params
+    )
+
+    with mock_emr_client_stubber:
+        response = StepFunctionsRunner(aws_stepfunctions_endpoint_url).run(
+            definition=definition, input_string=input_string
+        )
+
+    assert "SUCCEEDED" == response["status"]
+    assert ["j-0000000", "j-0000001", "j-0000002"] == json.loads(response["output"])["emr_cluster_ids"]
+    mock_emr_client_stubber.assert_no_pending_responses()
